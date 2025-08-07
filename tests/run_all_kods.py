b9c3f7e189e88766ade3add8243b729ea32a1690
dinamikfiyatpublic3 <dinamikfiyatpublic3@outlook.com>
2025-08-07 21:36:07 +0300

    Update run_all_kods.py

--- tests/run_all_kods.py
+++ tests/run_all_kods.py
@@ -109,7 +109,7 @@ def trigger_group_1():
 def trigger_group_2():
     workflows_group_2 = [
         {"workflow_owner": "dinamikfyt5", "workflow_repo": "anlik_guncel", "workflow_name": "supabase_table_yap.yml"},
-        {"workflow_owner": "dinamikfyt6", "workflow_repo": "anlik_guncel", "workflow_name": "concurrent_run_api_best_sales.yml"},
+        {"workflow_owner": "dinamikfyt1", "workflow_repo": "anlik_guncel", "workflow_name": "concurrent_run_api_best_sales.yml"},
         {"workflow_owner": "dinamikfyt3", "workflow_repo": "anlik_guncel", "workflow_name": "scrape_api_urunlerim_rakipli_kalan_monitor.yml"}
     ]
     logging.info("Grup 2 Workflow'ları başlatılıyor...")
@@ -141,6 +141,34 @@ def trigger_group_3():
     for thread in threads:
         thread.join()
 
+def trigger_group_3_1():
+    workflows_group_3_1 = [
+        {"workflow_owner": "dinamikfyt5", "workflow_repo": "anlik_guncel", "workflow_name": "supabase_update_view_yeni_table.yml"}
+    ]
+    logging.info("Grup 3_1 Workflow'ları başlatılıyor...")
+    for workflow in workflows_group_3_1:
+        run_workflow(workflow["workflow_name"], workflow["workflow_owner"], workflow["workflow_repo"])
+        
+def trigger_group_3_2():
+    workflows_group_3_2 = [
+        {"workflow_owner": "dinamikfyt1", "workflow_repo": "anlik_guncel", "workflow_name": "otomatik_api_scrape_matrix.yml"},
+        {"workflow_owner": "dinamikfyt2", "workflow_repo": "anlik_guncel", "workflow_name": "otomatik_api_scrape_matrix.yml"},
+        {"workflow_owner": "dinamikfyt3", "workflow_repo": "anlik_guncel", "workflow_name": "otomatik_api_scrape_matrix.yml"},
+        {"workflow_owner": "dinamikfyt4", "workflow_repo": "anlik_guncel", "workflow_name": "otomatik_api_scrape_matrix.yml"},
+        {"workflow_owner": "dinamikfyt5", "workflow_repo": "anlik_guncel", "workflow_name": "otomatik_api_scrape_matrix.yml"},
+        {"workflow_owner": "dinamikfyt6", "workflow_repo": "anlik_guncel", "workflow_name": "otomatik_api_scrape_matrix.yml"},
+        {"workflow_owner": "dinamikfyt7", "workflow_repo": "anlik_guncel", "workflow_name": "otomatik_api_scrape_matrix.yml"}
+        
+    ]
+    logging.info("Grup 3_2 Workflow'ları başlatılıyor...")
+    threads = []
+    for workflow in workflows_group_3_2:
+        thread = Thread(target=run_workflow, args=(workflow["workflow_name"], workflow["workflow_owner"], workflow["workflow_repo"]))
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join()
+        
 def trigger_group_4():
     workflows_group_4 = [
         {"workflow_owner": "dinamikfyt1", "workflow_repo": "anlik_guncel", "workflow_name": "scrape_api_markalarım_rakipli.yml"},
@@ -160,7 +188,7 @@ def trigger_group_4():
         thread.start()
     for thread in threads:
         thread.join()
-
+        
 def trigger_group_5():
     workflows_group_5 = [
         {"workflow_owner": "dinamikfyt4", "workflow_repo": "anlik_guncel", "workflow_name": "supabase_run.yml"}
@@ -174,9 +202,11 @@ def start_groups():
     group_1_thread = Thread(target=trigger_group_1)
     group_2_thread = Thread(target=trigger_group_2)
     group_3_thread = Thread(target=trigger_group_3)
+    group_3_1_thread = Thread(target=trigger_group_3_1)
+    group_3_2_thread = Thread(target=trigger_group_3_2)
     group_4_thread = Thread(target=trigger_group_4)
     group_5_thread = Thread(target=trigger_group_5)
-
+    
     group_0_thread.start()
     group_0_thread.join() 
     group_1_thread.start()
@@ -185,6 +215,10 @@ def start_groups():
     group_2_thread.join()
     group_3_thread.start()
     group_3_thread.join()
+    group_3_1_thread.start()
+    group_3_1_thread.join()
+    group_3_2_thread.start()
+    group_3_2_thread.join()
     group_4_thread.start()
     group_4_thread.join()
     group_5_thread.start()
