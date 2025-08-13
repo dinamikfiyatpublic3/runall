d35149505e792f535d4b79cc8071b9bd9f125e70
dinamikfiyatpublic3 <dinamikfiyatpublic3@outlook.com>
2025-08-13 13:24:46 +0300

    Update run_all_kods_all_manuel.py

--- tests/run_all_kods_all_manuel.py
+++ tests/run_all_kods_all_manuel.py
@@ -79,6 +79,33 @@ def run_workflow(workflow_name, workflow_owner, workflow_repo):
     wait_for_workflow_to_start(workflow_owner, workflow_repo)
     wait_for_workflows_to_complete(workflow_owner, workflow_repo)
 
+def trigger_group_3_1():
+    workflows_group_3_1 = [
+        {"workflow_owner": "dinamikfyt5", "workflow_repo": "anlik_guncel", "workflow_name": "supabase_update_view_yeni_table.yml"}
+    ]
+    logging.info("Grup 3_1 Workflow'ları başlatılıyor...")
+    for workflow in workflows_group_3_1:
+        run_workflow(workflow["workflow_name"], workflow["workflow_owner"], workflow["workflow_repo"])
+        
+def trigger_group_3_2():
+    workflows_group_3_2 = [
+        {"workflow_owner": "dinamikfyt1", "workflow_repo": "anlik_guncel", "workflow_name": "otomatik_api_scrape_matrix.yml"},
+        {"workflow_owner": "dinamikfyt2", "workflow_repo": "anlik_guncel", "workflow_name": "otomatik_api_scrape_matrix.yml"},
+        {"workflow_owner": "dinamikfyt3", "workflow_repo": "anlik_guncel", "workflow_name": "otomatik_api_scrape_matrix.yml"},
+        {"workflow_owner": "dinamikfyt4", "workflow_repo": "anlik_guncel", "workflow_name": "otomatik_api_scrape_matrix.yml"},
+        {"workflow_owner": "dinamikfyt5", "workflow_repo": "anlik_guncel", "workflow_name": "otomatik_api_scrape_matrix.yml"},
+        {"workflow_owner": "dinamikfyt6", "workflow_repo": "anlik_guncel", "workflow_name": "otomatik_api_scrape_matrix.yml"},
+        {"workflow_owner": "dinamikfyt7", "workflow_repo": "anlik_guncel", "workflow_name": "otomatik_api_scrape_matrix.yml"}
+        
+    ]
+    logging.info("Grup 3_2 Workflow'ları başlatılıyor...")
+    threads = []
+    for workflow in workflows_group_3_2:
+        thread = Thread(target=run_workflow, args=(workflow["workflow_name"], workflow["workflow_owner"], workflow["workflow_repo"]))
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join()
         
 def trigger_group_4():
     workflows_group_4 = [
@@ -109,11 +136,16 @@ def trigger_group_5():
         run_workflow(workflow["workflow_name"], workflow["workflow_owner"], workflow["workflow_repo"])
 
 def start_groups():
-    
+
+    group_3_1_thread = Thread(target=trigger_group_3_1)
+    group_3_2_thread = Thread(target=trigger_group_3_2)
     group_4_thread = Thread(target=trigger_group_4)
     group_5_thread = Thread(target=trigger_group_5)
     
-    
+    group_3_1_thread.start()
+    group_3_1_thread.join()
+    group_3_2_thread.start()
+    group_3_2_thread.join()
     group_4_thread.start()
     group_4_thread.join()
     group_5_thread.start()
