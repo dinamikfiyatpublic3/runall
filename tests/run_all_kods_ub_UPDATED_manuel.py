fc1ddddd32779e0784ce447f0b809c85a2dc2bdb
dinamikfiyatpublic3 <dinamikfiyatpublic3@outlook.com>
2026-04-23 17:56:48 +0300

    Update run_all_kods_ub_UPDATED_manuel.py

--- tests/run_all_kods_ub_UPDATED_manuel.py
+++ tests/run_all_kods_ub_UPDATED_manuel.py
@@ -85,18 +85,6 @@ def run_workflow(workflow_name, workflow_owner, workflow_repo):
 # DEĞİŞİKLİK: workflow_repo artık "workflows-public"
 # ============================================
 
-def trigger_group_1_1():
-    workflows_group_1_1 = [
-        {"workflow_owner": "dinamikfiyatpublic3", "workflow_repo": "workflows-public", "workflow_name": "concurrent_run_ana_api_matrix_kalanlar_ub.yml"}     
-    ]
-    logging.info("Grup 1_1 Workflow'ları başlatılıyor...")
-    threads = []
-    for workflow in workflows_group_1_1:
-        thread = Thread(target=run_workflow, args=(workflow["workflow_name"], workflow["workflow_owner"], workflow["workflow_repo"]))
-        threads.append(thread)
-        thread.start()
-    for thread in threads:
-        thread.join()
 
 def trigger_group_2():
     workflows_group_2 = [
@@ -189,7 +177,6 @@ def trigger_group_6():
         run_workflow(workflow["workflow_name"], workflow["workflow_owner"], workflow["workflow_repo"])
 
 def start_groups():
-    group_1_1_thread = Thread(target=trigger_group_1_1)
     group_2_thread = Thread(target=trigger_group_2)
     group_3_thread = Thread(target=trigger_group_3)
     group_3_1_thread = Thread(target=trigger_group_3_1)
@@ -199,8 +186,6 @@ def start_groups():
     group_5_thread = Thread(target=trigger_group_5)
     group_6_thread = Thread(target=trigger_group_6)
     
-    group_1_1_thread.start()
-    group_1_1_thread.join()  
     group_2_thread.start()
     group_2_thread.join()
     group_3_thread.start()
